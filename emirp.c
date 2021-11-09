764182f1ff5745c4085b2fe5d4b195bcb727636d
adnanali-in <adnanali.in@gmail.com>
2021-11-09 17:16:07 +0530

    EMIRP Numbers

--- emirp.c
+++ emirp.c
@@ -2,49 +2,72 @@
 
 int prime(int n);
 int reverse(int x);
+int palindrome(int y);
+
 void main()
 {
-    int n, temp_1, i, temp_2, new, new_2;
-    printf("Enter a Number to check if it's Emirp or Not\n");
-    scanf("%d", &n);
-    temp_1=n;
-    printf("Value of new is %d\n",temp_1);
-    new=prime(n);
-    if (new != 0)
+    char e;
+    int n, i, temp_1, temp_2, new, new_2;
+    printf("Enter a Number to check if it's EMIRP PRIME or Not\n");
+    while (!scanf("%d", &n))
     {
-        temp_2 = reverse(temp_1);
-        printf("Value of New 2 is %d",temp_2);
-        new_2=prime(temp_2);
+        printf("Invalid input. Please enter an integer value\a\n");
+        while ((e = getchar()) != '\n')
+            ;
+    }
+    if (palindrome(n) == 0 && n > 9)
+    {
+        temp_1 = n;
+        new = prime(n);
+        if (new != 0)
+        {
+            temp_2 = reverse(temp_1);
+            new_2 = prime(temp_2);
 
-        if(new==new_2){
-            printf("Given Number %d is A EMIRP and opposite is %d \n",temp_1,temp_2);
+            if (new == new_2)
+            {
+                printf("Given Number %d is an EMIRP PRIME \n", temp_1);
+            }
+            else
+            {
+                printf("Not an EMIRP Prime Number\n");
+            }
         }
-    
+        else
+            printf("The Entered Number is a Composite Number\n");
+    }
+    else
+    {
+        printf("Enter a Valid Positive Integer\n");
     }
 }
 
 int prime(int n)
 {
-    int flag = 0, temp;
-    temp = n;
-    for (int i = 2; i < n; i++)
+    int flag = 0;
+    if (n == 1)
     {
-        if (n % i == 0)
+        printf("1 is Neither Prime nor Composite\n");
+    }
+    else if (n > 1)
+    {
+        for (int i = 2; i < n; i++)
         {
-            flag = 1;
-            printf("%d\n", flag);
-            break;
+            if (n % i == 0)
+            {
+                flag = 1;
+                break;
+            }
         }
-    }
-    if (flag != 1)
-    {
-        printf("It is Prime\n");
-        return 1;
-    }
-    else if (flag == 0)
-    {
-        printf("Its not Prime\n");
-        return 0;
+        if (flag == 0)
+        {
+            return 1;
+        }
+        else if (flag == 1)
+        {
+
+            return 0;
+        }
     }
 }
 
@@ -57,6 +80,24 @@ int reverse(int x)
         rev = rev * 10 + remainder;
         x = x / 10;
     }
-    printf("Reverse is %d\n",rev);
     return rev;
+}
+
+int palindrome(int y)
+{
+
+    int temp, rev;
+    temp = y;
+    rev = reverse(y);
+    if (y >= 2 && y < 9)
+    {
+        printf("1-Digit Numbers are Not EMIRP primes\n");
+    }
+    else if (rev == temp)
+    {
+        printf("It is a Palindrome and Palindromes are Not EMIRP primes\n");
+        return 404;
+    }
+    else
+        return 0;
 }
